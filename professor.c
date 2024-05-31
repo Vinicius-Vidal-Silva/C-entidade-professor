095e2b6c5d9aa1b949833535696ff434877e7f0d
rafael-de-almeida <rafael.almeida.ofical04@gmail.com>
2024-05-31 19:01:42 -0300

    commit: estrutura de menus baseado no notion do parceiro

--- professor.c
+++ professor.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <string.h> // manipulação de string
 #include <stdlib.h> // limpar terminal e pausa no sistema
+int materia();
+
+int presenca();
+
+int atribuicaoNotas();
+
+int preencherNotas();
 
 int main(){
-	int opcao;
+	int opcao, sair;
 	char confirmacao;
-	int sair;
 	sair = 0;
-	
 	printf("**************************************");
 	printf("\nCase precise de ajuda digite: /ajuda");
 	printf("\n**************************************");
@@ -16,7 +21,7 @@ int main(){
 	printf("\nBem vindo {Professor}.\nO que deseja fazer hoje?");
 	
 	do{
-		printf("\n1 - Definir avaliacoes\n2 - Atribuir notas\n3 - Realizar chamada\n4 - Fechamento da materia\n5 - Sair");
+		printf("\n1 - Materias\n2 - Sair");
 		printf("\nEntre com a opcao desejada:");
 		scanf("%i", &opcao);
 		fflush(stdin);
@@ -28,30 +33,13 @@ int main(){
 				system("cls");
 				break;
 			case 1:
-				printf("Avaliacoes\n");
+				printf("Materias\n");
 				system("echo \"Pressione qualquer tecla para continuar\"");
-				getchar();
+                getchar();
 				system("cls");
+                materia();
 				break;
 			case 2:
-				printf("Notas\n");
-				system("echo \"Pressione qualquer tecla para continuar\"");
-				getchar();
-				system("cls");
-				break;
-			case 3:
-				printf("Chamada\n");
-				system("echo \"Pressione qualquer tecla para continuar\"");
-				getchar();
-				system("cls");
-				break;
-			case 4:
-				printf("Fechamento\n");
-				system("echo \"Pressione qualquer tecla para continuar\"");
-				getchar();
-				system("cls");
-				break;
-			case 5:
 				printf("Deseja realmente sair? (S/N): ");
 				scanf(" %c", &confirmacao);
 				fflush(stdin);
@@ -64,5 +52,205 @@ int main(){
 				break;
 		}
 	}while(sair == 0);
-	
+}
+int materia(){
+    int opcao, sair;
+    char confirmacao;
+    sair = 0;
+    printf("**************************************");
+    printf("\nCase precise de ajuda digite: /ajuda");
+    printf("\n**************************************");
+    printf("\n");
+    do{
+        printf("\n1 - Definir Presencas\n2 - Atribuicao das notas (Forma avaliativa)\n3 - Prencher notas\n4 - Voltar");
+        printf("\nEntre com a opcao desejada:");
+        scanf("%i", &opcao);
+        fflush(stdin);
+        switch (opcao){
+            default:
+                printf("Opcao invalida, tente novamente!\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                break;
+            case 1:
+                printf("Presenca\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                presenca();
+                break;
+            case 2:
+                printf("atribuir notas\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                atribuicaoNotas();
+                break;
+            case 3:
+                printf("preencher notas\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                preencherNotas();
+                break;
+            case 4:
+                printf("Deseja realmente sair? (S/N): ");
+                scanf(" %c", &confirmacao);
+                fflush(stdin);
+                if (confirmacao == 'S' || confirmacao == 's') {
+                    printf("Saindo...\n");
+                    sair = 1;
+                } else {
+                    system("cls");
+                }
+                break;
+        }
+    }while(sair == 0);
+    return 0;
+}
+
+int presenca() {
+    int opcao, sair;
+    char confirmacao;
+    sair = 0;
+    printf("**************************************");
+    printf("\nCase precise de ajuda digite: /ajuda");
+    printf("\n**************************************");
+    printf("\n");
+    do{
+        printf("\n1 - Definir Chamada\n2 - ver as faltas\n3 - alterar presencas\n4 - voltar");
+        printf("\nEntre com a opcao desejada:");
+        scanf("%i", &opcao);
+        fflush(stdin);
+        switch (opcao){
+            default:
+                printf("Opcao invalida, tente novamente!\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                break;
+            case 1:
+                printf("Chamada\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                presenca();
+                system("cls");
+                break;
+            case 2:
+                printf("ver as faltas\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                break;
+            case 3:
+                printf("alterar presencas\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                break;
+            case 4:
+                printf("Deseja realmente sair? (S/N): ");
+                scanf(" %c", &confirmacao);
+                fflush(stdin);
+                if (confirmacao == 'S' || confirmacao == 's') {
+                    printf("Saindo...\n");
+                    sair = 1;
+                } else {
+                    system("cls");
+                }
+                break;
+        }
+    }while(sair == 0);
+    return 0;
+}
+int atribuicaoNotas(){
+    int opcao, sair;
+    char confirmacao;
+    sair = 0;
+    printf("**************************************");
+    printf("\nCase precise de ajuda digite: /ajuda");
+    printf("\n**************************************");
+    printf("\n");
+    do{
+        printf("\n1 - adicionar forma avaliativa\n2 - editar forma avaliativa\n3 - voltar");
+        printf("\nEntre com a opcao desejada:");
+        scanf("%i", &opcao);
+        fflush(stdin);
+        switch (opcao){
+            default:
+                printf("Opcao invalida, tente novamente!\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                break;
+            case 1:
+                printf("Forma Avaliativa\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                presenca();
+                system("cls");
+                break;
+            case 2:
+                printf("editar forma avaliativa\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                break;
+            case 3:
+                printf("Deseja realmente sair? (S/N): ");
+                scanf(" %c", &confirmacao);
+                fflush(stdin);
+                if (confirmacao == 'S' || confirmacao == 's') {
+                    printf("Saindo...\n");
+                    sair = 1;
+                } else {
+                    system("cls");
+                }
+                break;
+        }
+    }while(sair == 0);
+    return 0;
+}
+int preencherNotas(){
+    int opcao, sair;
+    char confirmacao;
+    sair = 0;
+    printf("**************************************");
+    printf("\nCase precise de ajuda digite: /ajuda");
+    printf("\n**************************************");
+    printf("\n");
+    do{
+        printf("\n1 - Definir Chamada\n2 - voltar");
+        printf("\nEntre com a opcao desejada:");
+        scanf("%i", &opcao);
+        fflush(stdin);
+        switch (opcao){
+            default:
+                printf("Opcao invalida, tente novamente!\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                break;
+            case 1:
+                printf("Chamada\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                presenca();
+                system("cls");
+                break;
+            case 2:
+                printf("Deseja realmente sair? (S/N): ");
+                scanf(" %c", &confirmacao);
+                fflush(stdin);
+                if (confirmacao == 'S' || confirmacao == 's') {
+                    printf("Saindo...\n");
+                    sair = 1;
+                } else {
+                    system("cls");
+                }
+                break;
+        }
+    }while(sair == 0);
+ return 0;
 }
