afee8c3de8d2cda99c8241f7b011eff4236714ce
Vinicius_Vidal <viniciusvrdsilva@gmail.com>
2024-06-03 15:04:44 -0300

    menu atualizado conforme notion e adicionado algumas funcionalidades

--- professor/professor.c
+++ professor/professor.c
@@ -1,42 +1,298 @@
 #include <stdio.h>
-#include <string.h> // manipulação de string
+#include <string.h> // manipula??o de string
 #include <stdlib.h> // limpar terminal e pausa no sistema
+int materia();
 
-//função para pausar o programa
-void pausar(){
-	printf("Pressione qualquer tecla para continuar...");
-	getchar();
+int presenca();
+
+int atribuicaoNotas();
+
+int preencherNotas();
+
+void exibirAjuda();
+
+void definirAvaliacoes();
+
+void editarAvaliacoes();
+
+int main(){
+	int opcao, sair;
+	char confirmacao;
+	sair = 0;
+	printf("**************************************");
+	printf("\nCase precise de ajuda digite: /ajuda");
+	printf("\n**************************************");
+	printf("\n");
+	
+	printf("\nBem vindo {Professor}.\nO que deseja fazer hoje?");
+	
+	do{
+		printf("\n1 - Materias\n2 - Sair");
+		printf("\nEntre com a opcao desejada:");
+		scanf("%i", &opcao);
+		fflush(stdin);
+		switch (opcao){
+			default:
+				printf("Opcao invalida, tente novamente!\n");
+				system("echo \"Pressione qualquer tecla para continuar\"");
+				getchar();
+				system("cls");
+				break;
+			case 1:
+				printf("Materias\n");
+				system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+				system("cls");
+                materia();
+				break;
+			case 2:
+				printf("Deseja realmente sair? (S/N): ");
+				scanf(" %c", &confirmacao);
+				fflush(stdin);
+				if (confirmacao == 'S' || confirmacao == 's') {
+					printf("Saindo...\n");
+					sair = 1;
+				} else {
+					system("cls");
+				}
+				break;
+		}
+	}while(sair == 0);
+}
+int materia(){
+    int opcao, sair;
+    char confirmacao;
+    sair = 0;
+    printf("**************************************");
+    printf("\nCase precise de ajuda digite: /ajuda");
+    printf("\n**************************************");
+    printf("\n");
+    do{
+        printf("\n1 - Definir Presencas\n2 - Atribuicao das notas (Forma avaliativa)\n3 - Prencher notas\n4 - Voltar");
+        printf("\nEntre com a opcao desejada:");
+        scanf("%i", &opcao);
+        fflush(stdin);
+        switch (opcao){
+            default:
+                printf("Opcao invalida, tente novamente!\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                break;
+            case 1:
+                printf("Presenca\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                presenca();
+                break;
+            case 2:
+                printf("atribuir notas\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                atribuicaoNotas();
+                break;
+            case 3:
+                printf("preencher notas\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                preencherNotas();
+                break;
+            case 4:
+                printf("Deseja realmente sair? (S/N): ");
+                scanf(" %c", &confirmacao);
+                fflush(stdin);
+                if (confirmacao == 'S' || confirmacao == 's') {
+                    printf("Saindo...\n");
+                    sair = 1;
+                } else {
+                    system("cls");
+                }
+                break;
+        }
+    }while(sair == 0);
+    return 0;
 }
 
+int presenca() {
+    int opcao, sair;
+    char confirmacao;
+    sair = 0;
+    printf("**************************************");
+    printf("\nCase precise de ajuda digite: /ajuda");
+    printf("\n**************************************");
+    printf("\n");
+    do{
+        printf("\n1 - Definir Chamada\n2 - ver as faltas\n3 - alterar presencas\n4 - voltar");
+        printf("\nEntre com a opcao desejada:");
+        scanf("%i", &opcao);
+        fflush(stdin);
+        switch (opcao){
+            default:
+                printf("Opcao invalida, tente novamente!\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                break;
+            case 1:
+                printf("Chamada\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+				presenca();
+                system("cls");
+                break;
+            case 2:
+                printf("ver as faltas\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                break;
+            case 3:
+                printf("alterar presencas\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                break;
+            case 4:
+                printf("Deseja realmente sair? (S/N): ");
+                scanf(" %c", &confirmacao);
+                fflush(stdin);
+                if (confirmacao == 'S' || confirmacao == 's') {
+                    printf("Saindo...\n");
+                    sair = 1;
+                } else {
+                    system("cls");
+                }
+                break;
+        }
+    }while(sair == 0);
+    return 0;
+}
+int atribuicaoNotas(){
+    int opcao, sair;
+    char confirmacao;
+    sair = 0;
+    printf("**************************************");
+    printf("\nCase precise de ajuda digite: /ajuda");
+    printf("\n**************************************");
+    printf("\n");
+    do{
+        printf("\n1 - adicionar forma avaliativa\n2 - editar forma avaliativa\n3 - voltar");
+        printf("\nEntre com a opcao desejada:");
+        scanf("%i", &opcao);
+        fflush(stdin);
+        switch (opcao){
+            default:
+                printf("Opcao invalida, tente novamente!\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                break;
+            case 1:
+                printf("Forma Avaliativa\n");
+                definirAvaliacoes();
+				system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+				//presenca();
+                system("cls");
+                break;
+            case 2:
+                printf("editar forma avaliativa\n");
+                editarAvaliacoes();
+				system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                break;
+            case 3:
+                printf("Deseja realmente sair? (S/N): ");
+                scanf(" %c", &confirmacao);
+                fflush(stdin);
+                if (confirmacao == 'S' || confirmacao == 's') {
+                    printf("Saindo...\n");
+                    sair = 1;
+                } else {
+                    system("cls");
+                }
+                break;
+        }
+    }while(sair == 0);
+    return 0;
+}
+int preencherNotas(){
+    int opcao, sair;
+    char confirmacao;
+    sair = 0;
+    printf("**************************************");
+    printf("\nCase precise de ajuda digite: /ajuda");
+    printf("\n**************************************");
+    printf("\n");
+    do{
+        printf("\n1 - Definir Chamada\n2 - voltar");
+        printf("\nEntre com a opcao desejada:");
+        scanf("%i", &opcao);
+        fflush(stdin);
+        switch (opcao){
+            default:
+                printf("Opcao invalida, tente novamente!\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                system("cls");
+                break;
+            case 1:
+                printf("Chamada\n");
+                system("echo \"Pressione qualquer tecla para continuar\"");
+                getchar();
+                presenca();
+                system("cls");
+                break;
+            case 2:
+                printf("Deseja realmente sair? (S/N): ");
+                scanf(" %c", &confirmacao);
+                fflush(stdin);
+                if (confirmacao == 'S' || confirmacao == 's') {
+                    printf("Saindo...\n");
+                    sair = 1;
+                } else {
+                    system("cls");
+                }
+                break;
+        }
+    }while(sair == 0);
+ return 0;
+}
 //função para exibir a ajuda
-void exibir_ajuda() {
+void exibirAjuda() {
     printf("\n**************************************\n");
     printf("Ajuda do Programa:\n");
     printf("1 - Definir avaliacoes: Permite definir os criterios de avaliacao.\n");
     printf("	1.1 - Avaliações: Define a quantida de avaliações que desejar.\n");
-    printf("	1.2 - Peso*: Escolha o peso que deseja para as avaliacoes.\n");
-    printf("	*Obs.: Por padrao os pesos sao definidos como 1.\n");
+    printf("		1.1.1 - Peso*: Escolha o peso que deseja para as avaliacoes.\n");
+    printf("	1.2 - Alterar avaliações: Altera as avaliações existentes");
+	printf("	*Obs.: Por padrao os pesos sao definidos como 1.\n");
     printf("2 - Atribuir notas: Permite atribuir notas aos alunos.\n");
     printf("3 - Realizar chamada: Permite fazer a chamada dos alunos.\n");
     printf("4 - Fechamento da materia: Permite realizar o fechamento da materia.\n");
     printf("5 - Sair: Encerra o programa.\n");
     printf("A qualquer momento, digite '/ajuda' para exibir esta mensagem.\n");
 	printf("**************************************\n");
-	pausar();
+	system("echo \"Pressione qualquer tecla para continuar...\"");
 }
 
-void definir_avaliacoes(){
+//função para editar avaliações existentes
+void definirAvaliacoes(){
 	//abre o arquivo de avaliações
 	FILE *file = fopen("avaliacoes.txt", "r+");
 	if(!file){
-		//se o arquivo não existir, entra na condicionar
+		//se o arquivo não existir, entra na condicional
 		//e cria o arquivo
 		file = fopen("avaliacoes.txt", "w+");
 		if(!file){
 			//caso não abra o arquivo
 			//avisa ao usuário
 			printf("Erro ao abrir o arquivo!");
-			pausar();
+			system("echo \"Pressione qualquer tecla para continuar...\"");
 			return;
 		}
 	}
@@ -68,7 +324,7 @@ void definir_avaliacoes(){
 			//compara a entrada com '/ajuda' se for igual exibe
 			// a função ajuda
             if (strcmp(entrada, "/ajuda") == 0) {
-                exibir_ajuda();
+                exibirAjuda();
                 continue;
             }
 			
@@ -94,7 +350,7 @@ void definir_avaliacoes(){
 			//compara a entrada com '/ajuda' se for igual exibe
 			// a função ajuda
             if (strcmp(entrada, "/ajuda") == 0) {
-                exibir_ajuda();
+                exibirAjuda();
                 continue;
             }
 			
@@ -125,7 +381,7 @@ void definir_avaliacoes(){
 					//compara a entrada com '/ajuda' se for igual exibe
 					//a função ajuda
                     if (strcmp(entrada, "/ajuda") == 0) {
-                        exibir_ajuda();
+                        exibirAjuda();
                         continue;
                     }
 					
@@ -169,7 +425,7 @@ void definir_avaliacoes(){
 			//compara a entrada com '/ajuda' se for igual exibe
 			//a função ajuda
             if (strcmp(linha, "/ajuda") == 0) {
-                exibir_ajuda();
+                exibirAjuda();
                 continue;
             }
 
@@ -191,7 +447,7 @@ void definir_avaliacoes(){
                 entrada[strcspn(entrada, "\n")] = 0;
 
                 if (strcmp(entrada, "/ajuda") == 0) {
-                    exibir_ajuda();
+                    exibirAjuda();
                     continue;
                 }
 
@@ -207,7 +463,7 @@ void definir_avaliacoes(){
                 entrada[strcspn(entrada, "\n")] = 0;
 
                 if (strcmp(entrada, "/ajuda") == 0) {
-                    exibir_ajuda();
+                    exibirAjuda();
                     continue;
                 }
 
@@ -228,7 +484,7 @@ void definir_avaliacoes(){
                         entrada[strcspn(entrada, "\n")] = 0;
 
                         if (strcmp(entrada, "/ajuda") == 0) {
-                            exibir_ajuda();
+                            exibirAjuda();
                             continue;
                         }
 
@@ -250,90 +506,124 @@ void definir_avaliacoes(){
     }
 
     fclose(file);
-    pausar();
+	system("echo \"Pressione qualquer tecla para continuar\"");
 }
 
-int main(){
-	int opcao;
-	int sair;
-	char confirmacao;
-	char entrada[10];
-	sair = 0;
-	
-	printf("**************************************");
-	printf("\nCase precise de ajuda digite: /ajuda");
-	printf("\n**************************************");
-	printf("\n");
-	
-	printf("\nBem vindo {Professor}.\nO que deseja fazer hoje?");
-	
-	do{
-		printf("\n1 - Definir avaliacoes\n2 - Atribuir notas\n3 - Realizar chamada\n4 - Fechamento da materia\n5 - Sair");
-		printf("\nEntre com a opcao desejada ou digite /ajuda:");
-		
-		fgets(entrada,sizeof(entrada),stdin);//Le a entrada, nesse caso, entrada do teclado
-		//fgets(array a ser 'lido',tamanho do array, fluxo da onde vem a entrada(teclado))
-		
-		//procura o caractere de nova linha "\n"
-		//ao achar, retorna o indice da 1ª ocorrência
-		//'= 0' substitui por caractere nulo '\0'
-		//removendo o caractere de nova linha do fgets()
-		entrada[strcspn(entrada, "\n")] = 0;
-		
-		//strcmp compara duas strings
-		//no caso a entrada com '/ajuda'
-		//a função retorna 3 possibilidade
-		//0 se forem iguais
-		//< 0 se entrada menor que '/ajuda'
-		//> 0 se entrada maior que '/ajuda'
-		if (strcmp(entrada, "/ajuda") == 0) {
-    		//se for 0 exibe a ajuda
-			exibir_ajuda();
-    		continue;
-		}
-		
-		//função atoi() converte uma string em um inteiro
-		//converte entrada em inteiro
-		opcao = atoi(entrada);
-		
-		switch (opcao){
-			default:
-				printf("Opcao invalida, tente novamente!\n");
-				pausar();
-				system("cls");
-				break;
-			case 1:
-				printf("Avaliacoes\n");
-				definir_avaliacoes();
-				system("cls");
-				break;
-			case 2:
-				printf("Notas\n");
-				pausar();
-				system("cls");
-				break;
-			case 3:
-				printf("Chamada\n");
-				pausar();
-				system("cls");
-				break;
-			case 4:
-				printf("Fechamento\n");
-				pausar();
-				system("cls");
-				break;
-			case 5:
-				printf("Deseja realmente sair? (S/N): ");
-				scanf(" %c", &confirmacao);
-				fflush(stdin);
-				if (confirmacao == 'S' || confirmacao == 's') {
-					printf("Saindo...\n");
-					sair = 1;
-				} else {
-					system("cls");
-				}
-				break;
-		}
-	}while(sair == 0);
-	
+void editarAvaliacoes(){
+	//abre o arquivo de avaliações
+	FILE *file = fopen("avaliacoes.txt", "r+");
+	if(!file){
+		printf("Nenhuma avaliacao definida.\n");
+		system("echo \"Pressione qualquer tecla para continuar\"");
+	}	
+
+	// Arquivo não está vazio, exibir e perguntar se deseja alterar
+    //volta o ponteiro para o início do arquivo
+	rewind(file);
+    char linha[100];
+    //exibe as avaliações e pesos existentes no arquivo
+    printf("Avaliacoes definidas:\n");
+    //cria o loop para exibir todas as avaliações e pesos nos arquivo
+    while (fgets(linha, sizeof(linha), file)) {
+        printf("%s", linha);
+    }
+    
+    char alterar;
+    
+    //pergunta se o usuário deseja alterar os dados existentes
+	while (1) {
+    	printf("Deseja alterar as avaliacoes? (S/N): ");
+        //função para pegar a entrada do usuário
+        //3 parâmetros: variável de entrada, tamanho e método
+		fgets(linha, sizeof(linha), stdin);
+        //substitui o "\n" por um caracter nulo "\0"
+		linha[strcspn(linha, "\n")] = 0;
+
+		//compara a entrada com '/ajuda' se for igual exibe
+		//a função ajuda
+        if (strcmp(linha, "/ajuda") == 0) {
+            exibirAjuda();
+            continue;
+    	}
+
+    alterar = linha[0];
+    
+    if (alterar == 'S' || alterar == 's' || alterar == 'N' || alterar == 'n') break;
+
+    printf("Entrada inválida. Tente novamente.\n");
+    }
+
+    if (alterar == 'S' || alterar == 's') {
+        freopen("avaliacoes.txt", "w+", file);
+        int num_avaliacoes;
+        char definir_pesos;
+        char entrada[100];
+
+        while (1) {
+            printf("Defina o numero de avaliacoes: ");
+            fgets(entrada, sizeof(entrada), stdin);
+            entrada[strcspn(entrada, "\n")] = 0;
+
+            if (strcmp(entrada, "/ajuda") == 0) {
+                exibirAjuda();
+                continue;
+            }
+
+            num_avaliacoes = atoi(entrada);
+            if (num_avaliacoes > 0) break;
+
+            printf("Entrada inválida. Tente novamente.\n");
+        }
+
+        while (1) {
+            printf("Deseja definir pesos para as avaliacoes? (S/N): ");
+            fgets(entrada, sizeof(entrada), stdin);
+            entrada[strcspn(entrada, "\n")] = 0;
+
+            if (strcmp(entrada, "/ajuda") == 0) {
+                exibirAjuda();
+                continue;
+            }
+
+            definir_pesos = entrada[0];
+            if (definir_pesos == 'S' || definir_pesos == 's' || definir_pesos == 'N' || definir_pesos == 'n') break;
+
+            printf("Entrada inválida. Tente novamente.\n");
+        }
+
+        fprintf(file, "%d\n", num_avaliacoes);
+        for (int i = 1; i <= num_avaliacoes; i++) {
+            if (definir_pesos == 'S' || definir_pesos == 's') {
+                float peso;
+
+                while (1) {
+                    printf("Defina o peso da avaliacao %d: ", i);
+                    fgets(entrada, sizeof(entrada), stdin);
+                    entrada[strcspn(entrada, "\n")] = 0;
+
+                    if (strcmp(entrada, "/ajuda") == 0) {
+                        exibirAjuda();
+                        continue;
+                    }
+
+                    peso = atof(entrada);
+                    if (peso > 0) break;
+
+                    printf("Entrada inválida. Tente novamente.\n");
+                }
+
+                fprintf(file, "Avaliacao %d: %.2f\n", i, peso);
+            } else {
+                fprintf(file, "Avaliacao %d: 1\n", i); // Peso padrão 1
+            }
+        }
+        
+        printf("Avaliacoes redefinidas com sucesso!\n");
+        
+    } else {
+        printf("Manter as avaliacoes atuais.\n");
+    }
+    
+  	fclose(file);
+	system("echo \"Pressione qualquer tecla para continuar\"");
 }
