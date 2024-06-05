8ed2ae74b6e8de2d9ef546977eaf645e2e7bebb2
Vinicius_Vidal <viniciusvrdsilva@gmail.com>
2024-06-05 19:09:54 -0300

    aluno atualizado

--- aluno/mainB.c
+++ aluno/mainB.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//Nomes cadastrados no arquivo BIN: Joao, Maria, Jose, Pedro, Ana, Luis, Rita, Paula, Carlos e Julia
+
 // Definição da estrutura do registro com nome de tamanho variável
 typedef struct {
     size_t tamNome;  // Usar size_t para tamanhos
@@ -9,10 +9,10 @@ typedef struct {
     float p1;
     float p2;
     float media;
-} Aluno;
+} Estudante;
 
 // Função para ler um registro do arquivo binário
-int LerArquivo(FILE *file, Aluno *aluno) {
+int LerArquivo(FILE *file, Estudante *aluno) {
 
     if (fread(&aluno->tamNome, sizeof(size_t), 1, file) != 1) {
         return 0;
@@ -36,7 +36,7 @@ int LerArquivo(FILE *file, Aluno *aluno) {
 }
 
 // Função para pegar um registro específico do arquivo binário com base no nome e retornar os dados a partir de P1
-Aluno* EncontrarDadoPorNome(const char *nomeArquivo, const char *nome) {
+Estudante* EncontrarDadoPorNome(const char *nomeArquivo, const char *nome) {
 
     // Abre o arquivo binário para leitura (r de read e b de binário)
     FILE *file = fopen(nomeArquivo, "rb");
@@ -47,8 +47,8 @@ Aluno* EncontrarDadoPorNome(const char *nomeArquivo, const char *nome) {
         return NULL; // Retorna a função com o valor nulo
     }
 
-    Aluno temp; // Variável do tipo struct Aluno para comparar na função
-    Aluno *result = NULL; // Variável em q será guardado o resultado da função
+    Estudante temp; // Variável do tipo struct Estudante para comparar na função
+    Estudante *result = NULL; // Ponteiro em que será guardado o resultado da função
 
     // Enquanto a função LerArquivo retornar 1
     while (LerArquivo(file, &temp)) {
@@ -56,8 +56,8 @@ Aluno* EncontrarDadoPorNome(const char *nomeArquivo, const char *nome) {
         // Compara o valor da linha no arquivo binário com o nome que queremos encontrar
         if (strcmp(temp.nome, nome) == 0) {
 
-            // Result recebe o endereço de blocos consecutivos na memória que malloc (memory allocation) retorna, sendo convertido para a struct do tipo Aluno
-            result = (Aluno *)malloc(sizeof(Aluno));
+            // Result recebe o endereço de blocos consecutivos na memória que malloc (memory allocation) retorna, sendo convertido para a struct do tipo Estudante
+            result = (Estudante *)malloc(sizeof(Estudante));
 
             // Se o result for diferente de nulo
             if (result != NULL) {
@@ -71,14 +71,10 @@ Aluno* EncontrarDadoPorNome(const char *nomeArquivo, const char *nome) {
     fclose(file); // Fecha o arquivo
     return result; // Retorna dessa função o valor de result
 }
-
-int main() {
-
-    const char *fileTeste = "MateriaLP.bin"; // Constante do arquivo para teste
-    const char *nomeTeste = "Joao"; // Constante do nome a encontrar para teste
+void MostrarNotas(char *materia, char *nome){
 
     // Atribuindo a uma variável o valor da chamada da função EncontrarDadoPorNome
-    Aluno *aluno = EncontrarDadoPorNome(fileTeste, nomeTeste);
+    Estudante *aluno = EncontrarDadoPorNome(materia, nome);
 
     // Verifica se foi retornado algum valor pela função
     if (aluno != NULL) {
@@ -90,8 +86,52 @@ int main() {
     } else {
 
         // Imprime que o nome que queremos não foi encontrado
-        printf("Nome '%s' não encontrado no arquivo.\n", nomeTeste);
+        printf("Nome '%s' não encontrado no arquivo.\n", nome);
     }
+}
+void Aluno() {
+
+    int opcao = 0;
+
+    printf("\n\n--------- Bem vindo ao perfil de Aluno! ---------\n\n");
+
+    do
+    {
+        printf("--------- Digite a opcao desejada: --------------\n");
+        printf("------------------- 1 - Aviso -------------------\n");
+        printf("------------------- 2 - Notas -------------------\n");
+        printf("------------------- 3 - Faltas ------------------\n");
+        printf("------------------- 4 - Sair --------------------\n");
+        scanf("%i",&opcao);
+
+        switch (opcao)
+        {
+        case 1:
+            printf("Aviso será mostrado aqui!");
+            getchar();
+            getchar();
+            break;
+        case 2:
+            MostrarNotas("MateriaLP.bin", "Joao");
+            getchar();
+            getchar();
+            break;
+        case 4:
+            break;
+        default:
+            printf("Opcao Invalida");
+            break;
+        }
+
+    } while (opcao != 4);
+
+}
+
+int main() {
+
+    // Aqui ficaria o menu principal começando pelo Login
+    // E utilizando do privilegio no switch case pra acessar as funcoes
+    Aluno();
 
     // Fim do código
     system("Pause");
